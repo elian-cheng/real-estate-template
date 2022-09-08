c36d71b4a67a6b92c1442555eaa398f8a30f5d0c
Olga Chernega <chernega.o@gmail.com>
2022-09-09 00:20:26 +0300

    feat: add adaptive sections how and deals with tabs, using js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// Slider swiper main-block
+
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
@@ -7,4 +9,28 @@ const swiper = new Swiper(".swiper", {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
+});
+
+// Tabs in deals section
+const tabNavItems = document.querySelectorAll(".tabs-deals__button");
+const tabItems = document.querySelectorAll(".item-tabs");
+
+document.addEventListener("click", e => {
+  const targetElement = e.target;
+  let currentActiveIndex;
+  let newActiveIndex;
+  if (targetElement.closest(".tabs-deals__button")) {
+    tabNavItems.forEach((tabNavItem, index) => {
+      if (tabNavItem.classList.contains("active")) {
+        currentActiveIndex = index;
+        tabNavItem.classList.remove("active");
+      }
+      if (tabNavItem === targetElement) {
+        newActiveIndex = index;
+      }
+    });
+    targetElement.classList.add("active");
+    tabItems[currentActiveIndex].classList.remove("active");
+    tabItems[newActiveIndex].classList.add("active");
+  }
 });
